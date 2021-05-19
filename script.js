315079ad6ea9bdd9da3c9ad50e3317f3024634df
y.arcorio <y.arcorio@acreos.eu>
2021-05-19 22:29:41 +0200

    mise en place

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let response = await fetch("data.json");
-console.log(response)
-if (response.ok) {
-  // if HTTP-status is 200-299
-  // get the response body (the method explained below)
-  let json = await response.json();
-  alert("HTTP-success: " + json);
-} else {
-  alert("HTTP-Error: " + response.status);
-}
+// Replace ./data.json with your JSON feed
+fetch('data.json').then(response => {
+  return response.json();
+}).then(data => {
+  // Work with JSON data here
+  console.log(data);
+}).catch(err => {
+  // Do something for an error here
+});
