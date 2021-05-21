3007e88262d595e50e24c8d1fdc40a304dc52d0b
y.arcorio <y.arcorio@acreos.eu>
2021-05-21 15:43:51 +0200

    data for index page

--- src/js/query.js
+++ src/js/query.js
@@ -1,5 +1,5 @@
 export default async function retrieveContent() {
-  const url = "src/data.json";S
+  const url = "src/data.json";
   const response = await fetch(url);
   return response.json();
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,44 @@
 import retrieveContent from "./query.js";
 
+
+
 async function getTags() {
   try {
     const data = await retrieveContent();
     data.tags.forEach((element) => {
-      $("#tags").append("<li>" + element + "</li>");
+      $('#tags').append($('<li>' + element + '</li>').addClass('tag'));
+    });
+  } catch (e) {
+    console.log("Error", e);
+  }
+}
+
+async function getPhotographers() {
+  try {
+    const data = await retrieveContent();
+
+    data.photographers.forEach((photographer) => {
+      var urlImg = "src/Sample Photos/ID Photos/" + photographer.portrait;
+      var tags = "";
+      photographer.tags.forEach(tag => {
+        tags += '<span class="tag">' + tag + '</span>,'
+      })
+      $("#photographers").append(
+        $('<article></article>').addClass('card card_user').append(
+          $('<a></a>').addClass('link_user-profil').append(
+
+            $('<img></img>').attr('src', urlImg).addClass('img_user'),
+            $('<h2>' + photographer.name + '</h2>').addClass('name_user')
+          ),
+          $('<div></div>').addClass('text_user').append(
+            $('<p>' + photographer.city + ', ' + photographer.country + '</p>').addClass('loc_user'),
+            $('<p>' + photographer.tagline + '</p>').addClass('tagline_user'),
+            $('<p>' + photographer.price + '€/jour</p>').addClass('price_user')
+          ),
+          $('<div></div>').addClass('tags tags_user').append(
+            tags
+          )));
+
     });
   } catch (e) {
     console.log("Error", e);
@@ -12,3 +46,4 @@ async function getTags() {
 }
 
 getTags();
+getPhotographers();
